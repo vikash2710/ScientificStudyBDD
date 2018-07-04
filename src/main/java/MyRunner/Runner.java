5770d083ccb0cf2759ca743223efad2bd2db6c88
vikash2710 <vikash.gupta2710@gmail.com>
2018-07-04 20:48:02 +0530

    console output

--- src/main/java/MyRunner/Runner.java
+++ src/main/java/MyRunner/Runner.java
@@ -6,8 +6,12 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = ".\\src\\main\\java\\Features",
-		glue={"StepDefinition"}
+				features = ".\\src\\main\\java\\Features",
+				glue={"StepDefinition"},
+				format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
+				monochrome = true, //display the console output in a proper readable format
+				strict = true //it will check if any step is not defined in step definition file
+//				dryRun = false //to check the mapping is proper between feature file and step def file
 		
 		)
 public class Runner {
---
+++ test-outout/report.js
@@ -0,0 +1,194 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Login Feature",
+  "description": "",
+  "id": "login-feature",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "Login Scenario",
+  "description": "",
+  "id": "login-feature;login-scenario",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Scientific Study",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user selects role as \"\u003cRole\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user enters \"\u003cusername\u003e\" and \"\u003cpassword\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 14,
+  "name": "",
+  "description": "",
+  "id": "login-feature;login-scenario;",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password",
+        "Role"
+      ],
+      "line": 15,
+      "id": "login-feature;login-scenario;;1"
+    },
+    {
+      "cells": [
+        "admin",
+        "test@123",
+        "Staff"
+      ],
+      "line": 16,
+      "id": "login-feature;login-scenario;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "Login Scenario",
+  "description": "",
+  "id": "login-feature;login-scenario;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Scientific Study",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user selects role as \"Staff\"",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user enters \"admin\" and \"test@123\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.User_is_already_on_Login_Page()"
+});
+formatter.result({
+  "duration": 15162853485,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.title_of_login_page_is_Scientific_Study()"
+});
+formatter.result({
+  "duration": 39807964,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Staff",
+      "offset": 22
+    }
+  ],
+  "location": "LoginStepDefinition.user_selects_role_as(String)"
+});
+formatter.result({
+  "duration": 368144646,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "admin",
+      "offset": 13
+    },
+    {
+      "val": "test@123",
+      "offset": 25
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 715429538,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_clicks_on_login_button()"
+});
+formatter.result({
+  "duration": 24608614829,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_home_page()"
+});
+formatter.result({
+  "duration": 11083028,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.close_the_browser()"
+});
+formatter.result({
+  "duration": 877407450,
+  "status": "passed"
+});
+});
